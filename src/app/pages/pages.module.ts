51b92bb9f7677e619bd79a1f8a46f7b9bb83bbe9
Rafael Filgueiras <rafaelsilva@MacBook-Pro-de-Rafael.local>
2022-07-08 22:52:55 -0300

    first commit

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//modules
+import { RoutingModule } from './routing.module';
+
+//pages
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule
+  ]
+})
+export class PagesModule { }
